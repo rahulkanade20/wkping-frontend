e3c1c26c44bb1f8931d42608f7ca2085b0871e1a
Rahul.Kanade <Rahul.Kanade@wolterskluwer.com>
2025-11-10 17:44:03 +0530

    updating UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import RegisterLink from './components/RegisterLink';
